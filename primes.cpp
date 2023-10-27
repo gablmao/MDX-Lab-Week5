9c2dc685843dba3fd6036b0b3ce04cd7fa02dd1c
Gabriel Felip Mano Lasig <GM887@live.mdx.ac.uk>
2023-10-27 10:20:04 +0100

    initial commit

---
+++ primes.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+
+    cout << "Enter an integer: ";
+    int input=0;
+    cin >> input;
+
+    vector<int> nums;
+    
+
+    return 0;
+}
