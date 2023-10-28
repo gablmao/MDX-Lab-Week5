f0325d6f1bfe8c574c120ec28fb8564b1b67d4ef
Gabriel Felip Mano Lasig <GM887@live.mdx.ac.uk>
2023-10-28 01:47:46 +0100

    updated up to finding max and min

--- minmax.cpp
+++ minmax.cpp
@@ -4,7 +4,32 @@
 using namespace std;
 
 int main() {
-    cout << "Hello World" << endl;
+    //cout << "Hello World" << endl;
+
+    //task: generates 10 random numbers, store them in an array/vector, 
+    //find the minimum and maximum in array/vector
+
+
+    vector<int> minmax;
+    int generateNum;
+
+    //generate and store random numbers in vector
+    for (int i=0; i<10; i++) {
+        generateNum = rand() % 100 + 1;
+        minmax.push_back(generateNum);
+    }
+
+    //output the 10 random numbers
+    cout << "Program generated 10 random integers between 0 and 100: " << endl;
+    for (int i=0; i<minmax.size(); i++) {
+        cout << minmax[i] << " ";
+    }
+
+    //finds the min and max in vector
+    int max;
+    int min;
+    int compareVal;
+
 
     return 0;
 }
