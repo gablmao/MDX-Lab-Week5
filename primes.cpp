ec2673c95d15e7f624794705bed89ed003034ae7
Gabriel Felip Mano Lasig <GM887@live.mdx.ac.uk>
2023-10-28 01:31:23 +0100

    changed so that it uses bool to check for prime

--- primes.cpp
+++ primes.cpp
@@ -3,36 +3,45 @@
 
 using namespace std;
 
-int main() {
-
-    cout << "Enter an integer: ";
-    int input;
-    cin >> input;
-
-    vector<int> nums;
-
-    //creates list of numbers from 0 to inputted number
-    for (int i=0; i<=input; ++i) {
-        nums.push_back(i);
-    }
-
-    nums.erase(nums.begin(), nums.begin()+2);
 
     /* CHECK ITEMS IN LIST
 
     for (int i : nums) {
         cout << i << endl;
     }
-    
+
     */
 
+int main() {
+
+    cout << "Enter an integer: ";
+    int input;
+    cin >> input;
+
+    vector<int> primes;
 
 
-    //checks if number at [i] is prime to inputted number
-    //if not prime, remove from list
-    /*for (int i=2; i<(nums.size() / 2); ++i) {
 
-    }*/
+    for (int i=2; i<=input; i++) {  //initially assings prime bool to numbers 2 to input
+        bool prime = true;
+        for (int num=2; num <= i/2; num++) {  //checks if i is a prime
+            if (i % num == 0) {  //if it is true, it means the number i is divisible by more
+                prime = false;    //numbers than just 1 and itself
+                break;
+            }
+        }
+
+        if (prime) {
+            primes.push_back(i);
+        }
+    }
+
+
+
+    //print result
+    for (int n : primes) {
+        cout << n << endl;
+    }
 
     return 0;
 }
